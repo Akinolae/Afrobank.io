dcc716092317113fb31f5103e9a288927c23f579
Akinolae <makindeakinola22@gmail.com>
2023-03-10 19:04:53 +0100

    Update cards.ts and index.ts

--- src/@core/cards/cards.ts
+++ src/@core/cards/cards.ts
@@ -1,18 +1,16 @@
-import apiFunctionCall from "..";
-import { store, cards } from "../../@store/store";
+import { Cards } from "afrobank-sdk";
 import response from "../../@utils/response";
+import { store, cards as cardUpdate } from "../../@store/store";
+
+const cards = new Cards("http://localhost:3005/Api/v1/");
 
 const getCards = async (): Promise<void> => {
   try {
-    const res = await apiFunctionCall.apiFunctionCall({
-      url: "userCards",
-      method: "GET",
-      hasAuth: true,
-    });
+    const res = await cards.getCards();
 
     const data: any = response.extractData(res);
 
-    store.dispatch(cards(data?.message));
+    store.dispatch(cardUpdate(data?.message));
   } catch (error: any) {
     throw response.extractError(error);
   }
