742fcc2ac3904225485ca29b0e7888fb0ec04c70
Akinolae <makindeakinola22@gmail.com>
2023-04-30 09:56:42 +0100

    added new changes

--- src/@core/auth/auth.ts
+++ src/@core/auth/auth.ts
@@ -5,12 +5,12 @@ import {
   updateSignIn,
   update2faStatus,
 } from "../../@store/store";
-import { Auth } from "afrobank-sdk";
-import { LoginParams } from "afrobank-sdk/interface/index.interface";
+// import { Auth } from "afrobank-sdk";
+// import { LoginParams } from "afrobank-sdk/interface/index.interface";
 
-const auth = new Auth(store, "http://localhost:3005/Api/v1/", {
-  updateUser,
-});
+// const auth = new Auth(store, "http://localhost:3005/Api/v1/", {
+//   updateUser,
+// });
 
 const getToken = (): string | undefined => {
   const token = store.getState().user.payLoad.token;
@@ -21,14 +21,14 @@ const has2fa = (): boolean => {
   return store.getState().user.has2fasStatus;
 };
 
-const login = async (params: LoginParams) => {
+const login = async (params: any) => {
   try {
-    const res = await auth.login(params);
-    if (!res?.has2fa) {
-      store.dispatch(updateSignIn(true));
-    }
-    store.dispatch(updateUser(res));
-    return res?.has2fa;
+    // const res = await auth.login(params);
+    // if (!res?.has2fa) {
+    //   store.dispatch(updateSignIn(true));
+    // }
+    store.dispatch(updateUser({}));
+    // return res?.has2fa;
   } catch (error: any) {
     throw error.message;
   }
@@ -40,7 +40,7 @@ const isSignedIn = (): boolean => {
 
 const getProfile = async () => {
   try {
-    await auth.getProfile();
+    // await auth.getProfile();
   } catch (error: any) {
     throw error.message || error;
   }
@@ -48,9 +48,9 @@ const getProfile = async () => {
 
 const register = async (params: object) => {
   try {
-    await auth.register(params);
+    // await auth.register(params);
 
-    return;
+    return {};
   } catch (error: any) {
     throw error.message;
   }
@@ -58,7 +58,7 @@ const register = async (params: object) => {
 
 const validate2fa = async (params: string) => {
   try {
-    await auth.validate2FA(params);
+    // await auth.validate2FA(params);
 
     store.dispatch(update2faStatus(true));
     store.dispatch(updateSignIn(true));
--- src/@core/cards/cards.ts
+++ src/@core/cards/cards.ts
@@ -1,14 +1,14 @@
-import { Cards } from "afrobank-sdk";
+// import { Cards } from "afrobank-sdk";
 import response from "../../@utils/response";
 import { store, cards as cardUpdate } from "../../@store/store";
 
-const cards = new Cards("http://localhost:3005/Api/v1/");
+// const cards = new Cards("http://localhost:3005/Api/v1/");
 
 const getCards = async (): Promise<void> => {
   try {
-    const res = await cards.getCards();
+    // const res = await cards.getCards();
 
-    const data: any = response.extractData(res);
+    const data: any = response.extractData({});
 
     store.dispatch(cardUpdate(data?.message));
   } catch (error: any) {
